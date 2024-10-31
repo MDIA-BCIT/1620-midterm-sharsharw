d446a0b2c557bd480f94a662f04346e1cd2ffefe
sharsharw <swang322@my.bcit.ca>
2024-10-31 15:28:35 -0700

    Answer to problem.js

--- problem.js
+++ problem.js
@@ -13,3 +13,38 @@ CHALLENGE
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+
+/* Thought process:  
+- set userInput as a constant to ensure that it is the correct password 
+- if user input === password exactly then log out "Access Granted"
+- if user input !== password exactly then log out "Access Denied!"
+- if user input = "forgot", log out "Here is a hint"
+- if user input = "reset", log out "Let's reset your account"
+  - "forgot" and "reset" should be parameters 
+Challenge:
+- userInput.length < 5 then console.log("your password is too short")
+*/
+
+
+const userInput = "mdia1620";
+
+function passwordCheck(password){
+    if (userInput === password){
+       console.log("Access Granted"); 
+    } else if (userInput !== password){
+        console.log("Access Denied!");
+    } 
+}
+passwordCheck();
+
+// enter password at "passwordCheck()" to see if it matches  
+/* // Here I need to add in 2 more parameters where the userInput is forgot or reset. However, it needs to be defined.// 
+
+    } else if (forgot) {
+        console.log ("Here is a hint");
+    } else if (reset) {
+        console.log("Let's reset your account");
+    }
+}
+
+*/ 
